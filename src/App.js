47dd65eef260fc2c3710c36c88243ffed72bd776
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-09-26 19:56:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
  
 import './App.css';
 
-function App() {
+function evaluateGuess(){
+let userElement = document.getElementById('enter_num')
+let userval=userElement.value;
+console.log(userval)
 
+
+
+
+}
+
+
+
+
+function App() {
   // object in react
   const myintro={
   name:"Aniruddha Adak",
@@ -19,6 +31,7 @@ function App() {
     <>
     <center>
     <div className="div1">
+
       <h1 id='h11'>Hello react...</h1>
       <h2 id='h21'>I am learning React js from studytonioght ...</h2>
       <h2 id='h22'>My Introduction:</h2>
@@ -26,12 +39,29 @@ function App() {
       <p>I lived in : <b>{myintro.address}</b>.</p>
       <p>My roll no is : <b>{myintro.roll}</b> .</p>
       <p>My passion is : <b>{myintro.passion}</b>.</p>
-      
+  
+    </div>
+   
+<br/> 
+<br/> 
+
+ 
+
+
+
+    {/* 2nd div element for creating game from studytonight*/}
+
+    <div>
+      <h1 id='h12'>Number Guessing Game</h1>
+
+<div><input id='enter_num' type='text' placeholder='Please enter a number...'/> </div>
+<br/>
+<div><button id='verify' onClick={evaluateGuess}>Verify</button></div>
 
 
     </div>
-    </center>
-    
+
+     </center>
     </>  //end of fragment 
   );
 }
